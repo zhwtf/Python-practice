589bd914d93b6332eb8b1277528485bc71f17e5c
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-12-29 09:24:32 -0800

    v2

--- urllib_module.py
+++ urllib_module.py
@@ -1,5 +1,18 @@
 import urllib.request
+import urllib.parse
+#x = urllib.request.urlopen('https://www.google.com')
 
-x = urllib.request.urlopen('https://www.google.com')
+#print(x.read())
 
-print(x.read())
+url = 'http://pythonprogramming.net'
+values = {'s':'basic',
+            'submit':'search'}
+
+data = urllib.parse.urlencode(values)
+#the type of encoding utf-8
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+print(respData)
