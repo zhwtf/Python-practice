7c213fac6e4a46210b159a205a6c6d03ae5b9c6c
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-12-29 09:53:20 -0800

    v4

--- urllib_module.py
+++ urllib_module.py
@@ -3,7 +3,7 @@ import urllib.parse
 #x = urllib.request.urlopen('https://www.google.com')
 
 #print(x.read())
-
+'''
 url = 'http://pythonprogramming.net'
 values = {'s':'basic',
             'submit':'search'}
@@ -12,7 +12,31 @@ data = urllib.parse.urlencode(values)
 #the type of encoding utf-8
 data = data.encode('utf-8')
 req = urllib.request.Request(url, data)
+#this is Get
 resp = urllib.request.urlopen(req)
 respData = resp.read()
 
 print(respData)
+'''
+try:
+    x = urllib.request.urlopen('https://www.google.com/search?q=test')
+
+    print(x.read())
+except Exception as e:
+    print(str(e))
+
+try:
+    url = 'https://www.google.com/search?q=test'
+
+    headers = {}
+    headers['User-Agent'] = 'Mozilla/5.0'
+    req = urllib.request.Request(url, headers = headers)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+
+    saveFile = open('withHeaders.txt','w')
+    saveFile.write(str(respData))
+    saveFile.close()
+
+except Exception as e:
+    print(str(e))
