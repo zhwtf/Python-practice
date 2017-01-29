573739208b1aac6b7624c44833c1c8d4e8d49867
haozheng <zhwtf123@gmail.com>
2017-01-28 23:16:21 -0800

    28-00

---
+++ object_oriented_programming.py
@@ -0,0 +1,19 @@
+class Blob:
+
+    def __init__(self, color):
+        self.x = random.randrange(0, WIDTH)
+        self.y = random.randrange(0, HEIGHT)
+        self.size = random.randrange(4,8)
+        self.color = color
+
+    def move(self):
+        self.move_x = random.randrange(-1,2)
+        self.move_y = random.randrange(-1,2)
+        self.x += self.move_x
+        self.y += self.move_y
+
+        if self.x < 0: self.x = 0
+        elif self.x > WIDTH: self.x = WIDTH
+
+        if self.y < -: self.y = 0
+        elif self.y > HEIGHT: self.y = HEIGHT
