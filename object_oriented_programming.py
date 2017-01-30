092b573d72cd162e090fa9c1cc15bd03b3808ed0
haozheng <zhwtf123@gmail.com>
2017-01-29 23:50:49 -0800

    29-00

--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -1,3 +1,18 @@
+import pygame
+import random
+
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+game_display = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption("Blob World")
+clock = pygame.time.Clock()
+
+
+
 class Blob:
 
     def __init__(self, color):
